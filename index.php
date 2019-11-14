d758971ac4a205b4d81c23cd6a32bcdbff29c153
KhaosBlaze <khaosblaze@gmail.com>
2019-11-14 17:39:02 -0500

    L10.1

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
-<?php 
+<?php
 
-$task = [
-	'title' => 'Finish Homework',
-	'due' => 'today',
-	'assigned_to' => 'Charles',
-	'completed' => htmlspecialchars($_GET['comp'])
-];
+$animals = ['dog','cat'];
 
+function dd($variable){
+
+			echo 'pre';
+			die(var_dump($variable))
+			echo 'pre';
+
+}
+
+dd($animals)
+
+// echo '<pre>';
+// die (var_dump($animals));
+// echo '</pre>';
 
 require 'index.view.php';
 
