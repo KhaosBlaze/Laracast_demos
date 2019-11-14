674f2a024717c407bee954159e6598e7a7b0235f
KhaosBlaze <khaosblaze@gmail.com>
2019-11-14 18:33:37 -0500

    L12

--- index.php
+++ index.php
@@ -2,7 +2,27 @@
 
 require 'functions.php';
 
-$response = bouncer(htmlspecialchars($_GET['age']));
+// Todo Application
+
+//
+
+// Todo, Comment, User
+
+
+class Task {
+
+	protected $description
+	protected $completed = false;
+
+	public function __construct($description)
+	{
+			//Automatically Trigger on instantiation
+			$this->description = $description;
+	}
+
+}
+
+$task = new Task('Go to the store');
 
 require 'index.view.php';
 
