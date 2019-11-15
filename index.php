ffbe2b7a80612eeb8809355ec7b38ebcf8044ace
KhaosBlaze <khaosblaze@gmail.com>
2019-11-15 13:18:41 -0500

    L16.7

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $query = require 'core/bootstrap.php';
 
 $uri = trim(str_replace('Laracast/', '', $_SERVER['REQUEST_URI']), '/');
 
-$router = Router::load('routes.php');
-$router->direct($uri);
+require Router::load('routes.php')
+	->direct($uri);
 
 ?>
