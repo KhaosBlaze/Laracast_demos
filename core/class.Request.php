c82511bef1bf30f08b419f4edbc95f28baddad08
KhaosBlaze <khaosblaze@gmail.com>
2019-11-15 13:28:12 -0500

    L16.8

--- core/class.Request.php
+++ core/class.Request.php
@@ -1,12 +1,11 @@
 <?php
 
-$cofig = require 'config.php';
-
 class Request
 {
 
   public static function uri()
   {
+    $config = require 'config.php';
     return trim(str_replace($config['directory']."/", '', $_SERVER['REQUEST_URI']), '/');
   }
 }
