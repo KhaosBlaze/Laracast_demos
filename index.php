1386e3b45b6be128ea46a55d80c030d285b45284
KhaosBlaze <khaosblaze@gmail.com>
2019-11-14 20:31:46 -0500

    L13.3.4

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
 $query = require 'bootstrap.php';
 
 require 'class.Task.php';
+require 'functions.php';
 
 
 $tasks = $query->selectAll('todos', 'Task');
+
+dd($tasks);
 
 require 'index.view.php';
 ?>
