4a7f3b8e28730252d6e3ada777ae90b152ac0fb7
KhaosBlaze <khaosblaze@gmail.com>
2019-11-14 19:01:04 -0500

    L13.2

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
 
 try {
-new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'root', 'NMHouGAtpRsrsuyim2FR');
+	$pdo = new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'root', 'NMHouGAtpRsrsuyim2FR');
 } catch(PDException $e) {
 	die('Could not connect.');
 }
+
+$statement = $pdo->prepare('select * from todos');
+
+$statement-> execute();
+
+var_dump($statement->fetchALL())
 
 require 'index.view.php';
 ?>
