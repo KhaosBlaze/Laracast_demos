e25298a76d53a40d013bc0f1c5fcc5e9d652d51e
KhaosBlaze <khaosblaze@gmail.com>
2019-11-17 00:03:55 -0500

    L21.3

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -2,11 +2,11 @@
 
 $app = [];
 $app['config'] = require 'config.php';
-require 'core/database/QueryBuilder.php';
-require 'core/database/Connection.php';
-require 'core/class.Task.php';
-require 'core/class.Router.php';
-require 'core/class.Request.php';
+// require 'core/database/QueryBuilder.php';
+// require 'core/database/Connection.php';
+// require 'core/class.Task.php';
+// require 'core/class.Router.php';
+// require 'core/class.Request.php';
 
 $app['database'] =  new QueryBuilder(
   Connection::make($app['config']['database'])
